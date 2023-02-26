e90f77d94e5c85a58c3f6cde0e2c3bc19946349b
Jiseok Son <jiseokson@gmail.com>
2023-02-26 23:49:46 +0900

    react-todo-app 기능 완성

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,9 +1,89 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function TodoForm({ addTodo }) {
+  const [todoText, setTodoText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todoText.trim()) {
+      addTodo(todoText.trim());
+      setTodoText('');
+    }
+  };
+
   return (
-    <div className="App">
-      <h1>Todo app</h1>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter a new to-do..."
+        value={todoText}
+        onChange={(e) => setTodoText(e.target.value)}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+function TodoItem({ todo, toggleComplete, deleteTodo }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleComplete(todo.id)}
+      />
+      <span style={{ textDecoration: todo.completed ? 'line-through' : '' }}>
+        {todo.text}
+      </span>
+      <button onClick={() => deleteTodo(todo.id)}>X</button>
+    </li>
+  );
+}
+
+function TodoList({ todos, toggleComplete, deleteTodo }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          todo={todo}
+          toggleComplete={toggleComplete}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodo = { id: Date.now(), text, completed: false };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        toggleComplete={toggleComplete}
+        deleteTodo={deleteTodo}
+      />
     </div>
   );
 }
